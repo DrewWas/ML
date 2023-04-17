0f698817cb83f096804a6efe2732aae9e1417ac2
Andrew Wasielewski <awwasielewski@gmail.com>
2023-04-17 00:55:32 -0700

    done for the night

--- environments/upanddown.py
+++ environments/upanddown.py
@@ -6,19 +6,29 @@ WIN = pygame.display.set_mode((WID, HEIGHT))
 pygame.display.set_caption("flap bird fr")
 pygame.display.flip()
 
- # CONSTANTS
+# CONSTANTS
 blue = (0,138, 255)
 black = (0,0,0)
-#height = randint(-200,350)
+
 
 
 def bird(bird_y):
     return pygame.draw.rect(WIN, (200,100,100), (200, bird_y, 35,35))
 
 
-def bricks(x_pos, y_pos):
+def get_heights(T_or_F):
+    if T_or_F:
+        return [randint(0,500) for _ in range(10)]
     
-    blocks = [pygame.draw.rect(WIN, blue, (x_pos + (i * 200), y_pos, 100, 100)) for i in range(10)]    
+
+
+def bricks(x_pos, y_pos):
+
+    y_pos = get_heights(True)
+    
+    upper_blocks = [pygame.draw.rect(WIN, blue, (x_pos + (i * 200), 0, 50, y_pos[i])) for i in range(10)]
+    lower_blocks = [pygame.draw.rect(WIN, blue, (x_pos + (i * 200), (y_pos[i] + 150), 50, 800)) for i in range(10)]
+    return None 
 
 
 
@@ -27,21 +37,22 @@ def main():
     gameon = False
     brick_x = 400
     brick_y = 400
+    reset = False
 
     bird_y = 200
 
 
     while run:
         # Basic shit 
+        pygame.time.Clock().tick(60)
         pygame.display.update()
         WIN.fill(black)
-        pygame.time.Clock().tick(60)
         keys = pygame.key.get_pressed()
 
         # Movement functionality 
-        if keys[pygame.K_UP] and bird_y > 10:
+        if (keys[pygame.K_UP] or keys[pygame.K_w])and bird_y > 10:
                 bird_y -= 8
-        if keys[pygame.K_DOWN] and bird_y < 760 :
+        if (keys[pygame.K_DOWN] or keys[pygame.K_s]) and bird_y < 760 :
             bird_y += 8
 
         #Start game functionality 
@@ -49,8 +60,9 @@ def main():
             gameon = True
         
         if gameon:
-            brick_x -= 2
+            brick_x -= 5
 
+      
         bricks(brick_x, brick_y)
         bird(bird_y)
         for event in pygame.event.get():
