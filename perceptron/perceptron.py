5f64567410e54ce1abdac1859d64c18644744c20
Andrew Wasielewski <awwasielewski@gmail.com>
2025-01-14 04:08:38 -0500

    PERCEPTRONgit add .

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -27,10 +27,10 @@ def main():
 
             WINDOW.fill(BLACK)
 
-            # Weird math so that the origin is technically at the center
             real_pos = pygame.mouse.get_pos()
-            x_pos = pygame.mouse.get_pos()[0] - (WIDTH // 2) 
-            y_pos = (HEIGHT // 2) - pygame.mouse.get_pos()[1] 
+
+            x_pos = pygame.mouse.get_pos()[0] 
+            y_pos = pygame.mouse.get_pos()[1] 
 
             if event.type == pygame.QUIT:
                 run = False
@@ -45,6 +45,11 @@ def main():
                 if real_pos[0] > 20 and real_pos[0] < 190 and real_pos[1] > 95 and real_pos[1] < 130:   
                     data_type_selected = BLACK 
                     boundaries = train_weights()
+                    pygame.draw.line(WINDOW, GREEN, boundaries[0], boundaries[1], 5) 
+
+                if real_pos[0] > 20 and real_pos[0] < 190 and real_pos[1] > 135 and real_pos[1] < 170:   
+                    data_matrix.clear()
+                    weight_vector = [0.5, 0.5, 0.5]
 
                 elif data_type_selected == RED and (real_pos[0] > 200 or real_pos[1] > 140):
                     # RED = 0 for data purposes
@@ -61,13 +66,13 @@ def main():
             # Unfortunately we have to redraw because otherwise the mouse would just paint over the entire screen
             for point in data_matrix:
                 if point[3] == 0:
-                    pygame.draw.circle(WINDOW, RED, (point[1] + (WIDTH // 2), -point[2] + (HEIGHT // 2)), 10) 
+                    pygame.draw.circle(WINDOW, RED, (point[1], point[2]), 10)
                 elif point[3] == 1:
-                    pygame.draw.circle(WINDOW, BLUE, (point[1] + (WIDTH // 2), -point[2] + (HEIGHT // 2)), 10) 
+                    pygame.draw.circle(WINDOW, BLUE, (point[1], point[2]), 10) 
 
 
             pygame.draw.line(WINDOW, GREEN, boundaries[0], boundaries[1], 5) 
-            pygame.draw.circle(WINDOW, data_type_selected, (x_pos + (WIDTH // 2), -y_pos + (HEIGHT // 2)), 10) 
+            pygame.draw.circle(WINDOW, data_type_selected, (x_pos, y_pos), 10) 
             draw_color_select(x_pos, y_pos)
             pygame.display.update()            
     
@@ -82,13 +87,13 @@ def draw_grid_lines():
             pygame.draw.line(WINDOW, GRAY, (i * 25, 0), (i * 25, 1000)) 
             pygame.draw.line(WINDOW, GRAY, (0, i * 25), (1000, i * 25)) 
 
-    pygame.draw.line(WINDOW, GRAY, (WIDTH // 2, 0), (WIDTH // 2, 1000), 5) 
-    pygame.draw.line(WINDOW, GRAY, (0, HEIGHT // 2), (1000, HEIGHT // 2), 5) 
+    #pygame.draw.line(WINDOW, GRAY, (WIDTH // 2, 0), (WIDTH // 2, 1000), 5) 
+    #pygame.draw.line(WINDOW, GRAY, (0, HEIGHT // 2), (1000, HEIGHT // 2), 5) 
 
 
 def draw_color_select(x_pos, y_pos):
     # SELECTION DISPLAY
-    pygame.draw.rect(WINDOW, WHITE, pygame.Rect(10, 10, 190, 125))
+    pygame.draw.rect(WINDOW, WHITE, pygame.Rect(10, 10, 190, 165))
 
     pygame.draw.rect(WINDOW, BLUE, pygame.Rect(20, 15, 170, 33))
     WINDOW.blit(FONT.render("Blue Data", False, BLACK), (40, 10))
@@ -98,6 +103,9 @@ def draw_color_select(x_pos, y_pos):
     pygame.draw.rect(WINDOW, GREEN, pygame.Rect(20, 95, 170, 33), 3)
     WINDOW.blit(FONT.render("START", False, BLACK), (60, 92))
 
+    pygame.draw.rect(WINDOW, RED, pygame.Rect(20, 135, 170, 33), 3)
+    WINDOW.blit(FONT.render("RESET", False, BLACK), (60, 132))
+
 
     # COORDINATE DISPLAY
     pygame.draw.rect(WINDOW, WHITE, pygame.Rect(850, 10, 140, 30))
@@ -106,8 +114,8 @@ def draw_color_select(x_pos, y_pos):
 
 
 def train_weights():
-    learning_rate = 0.2
-    epochs = 100
+    learning_rate = 0.5
+    epochs = 1000
     w0, w1, w2 = weight_vector
 
     for epoch in range(epochs):
@@ -131,9 +139,8 @@ def train_weights():
                 weight_vector[j] += learning_rate * error * i[j] 
               
 
-    #return (0, (((500 * w1) + w0) / w2) + 500), (1000, (((1500 * w1) + w0) / w2) + 500)
-    #return (-1000, -(((-1000 * w1) + w0) / w2) + 500), (1000, -(((1000 * w1) + w0) / w2) + 500)
-    return (-1000, (((-1000 * w1) + w0) / w2) + 1000), (1000, (((1000 * w1) + w0) / w2) + 500)
+    # Something like this, y-int (bias) is not being taken into account really
+    return (-1000, -((-1000 * w1) + w0) / w2), (1000, -((1000 * w1) + w0) / w2)
 
 
 
